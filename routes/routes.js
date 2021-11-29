dcf89d2446378827119c573a6a048ad217352ef7
aiswaryark1 <aiswarya.radhakrishnan@experionglobal.com>
2021-11-29 15:00:48 +0530

    changes made

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const Routes = require('./route');
+const Routes1 = require('./Courseroute');
+const Routes2 = require('./Resourceroute');
+const Routes3 = require('./Enquiryroute');
+const Routes4 = require('./Usersroute');
 
-router.use('/courses',Routes);
-router.use('/resources',Routes);
-router.use('/users',Routes);
+router.use('/courses',Routes1);
+module.exports = router;
+router.use('/resources',Routes2);
+module.exports = router;
+router.use('/users',Routes4);
+module.exports = router;
+router.use('/enquiries',Routes3);
+
 module.exports = router;
