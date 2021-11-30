fd9c3046fdb344febbb194990a8b2597358e5f14
kiran010199 <kiran.giby@experionglobal.com>
2021-11-30 20:46:18 +0530

    edit

--- crm/src/AppReactBoot.js
+++ crm/src/AppReactBoot.js
@@ -1,72 +1,73 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import { MDBCard, MDBCardBody,MDBRow,MDBCol, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import { MDBCard, MDBCardBody,MDBRow,MDBCol, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
 
-export default function App() {
-  return (
-    <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-      <MDBCol>
-        <MDBCard className='h-100'>
-        <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
-            alt='...'
-            position='top'
-          />
-          </MDBRipple>
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-      <MDBCol>
-        <MDBCard className='h-100'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>This is a short card.</MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-      <MDBCol>
-        <MDBCard className='h-100'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-      <MDBCol>
-        <MDBCard className='h-100'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/044.jpg'
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    </MDBRow>
-  );
-}
+// export default function App() {
+//   return (
+//     <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+//       <MDBCol>
+//         <MDBCard className='h-100'>
+//         <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+//           <MDBCardImage
+//             src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
+//             alt='...'
+//             position='top'
+//           />
+//           </MDBRipple>
+//           <MDBCardBody>
+//             <MDBCardTitle>Card title</MDBCardTitle>
+//             <MDBCardText>
+//               This is a longer card with supporting text below as a natural lead-in to additional content.
+//               This content is a little bit longer.
+//             </MDBCardText>
+//           </MDBCardBody>
+//         </MDBCard>
+//       </MDBCol>
+//       <MDBCol>
+//         <MDBCard className='h-100'>
+//           <MDBCardImage
+//             src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
+//             alt='...'
+//             position='top'
+//           />
+//           <MDBCardBody>
+//             <MDBCardTitle>Card title</MDBCardTitle>
+//             <MDBCardText>This is a short card.</MDBCardText>
+//           </MDBCardBody>
+//         </MDBCard>
+//       </MDBCol>
+//       <MDBCol>
+//         <MDBCard className='h-100'>
+//           <MDBCardImage
+//             src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
+//             alt='...'
+//             position='top'
+//           />
+//           <MDBCardBody>
+//             <MDBCardTitle>Card title</MDBCardTitle>
+//             <MDBCardText>
+//               This is a longer card with supporting text below as a natural lead-in to additional content.
+//             </MDBCardText>
+//           </MDBCardBody>
+//         </MDBCard>
+//       </MDBCol>
+//       <MDBCol>
+//         <MDBCard className='h-100'>
+//           <MDBCardImage
+//             src='https://mdbcdn.b-cdn.net/img/new/standard/city/044.jpg'
+//             alt='...'
+//             position='top'
+//           />
+//           <MDBCardBody>
+//             <MDBCardTitle>Card title</MDBCardTitle>
+//             <MDBCardText>
+//               This is a longer card with supporting text below as a natural lead-in to additional content.
+//               This content is a little bit longer.
+//             </MDBCardText>
+//           </MDBCardBody>
+//         </MDBCard>
+//       </MDBCol>
+//     </MDBRow>
+//   );
+// }
+
