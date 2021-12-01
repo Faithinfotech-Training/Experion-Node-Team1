52cd4b1e0a4326c8cd90460b0be87f01f99bce85
aiswaryark1 <aiswarya.radhakrishnan@experionglobal.com>
2021-12-01 11:15:02 +0530

    API

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const Routes1 = require('./Courseroute');
 const Routes2 = require('./Resourceroute');
 const Routes3 = require('./Enquiryroute');
 const Routes4 = require('./Usersroute');
+const Routes5 = require('./Enquirystatusroute');
 
 router.use('/courses',Routes1);
 
@@ -12,5 +13,6 @@ router.use('/resources',Routes2);
 router.use('/users',Routes4);
 
 router.use('/enquiries',Routes3);
+router.use('/enquirystatus',Routes5);
 
 module.exports = router;
